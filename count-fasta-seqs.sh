91140674b1cf1c535f04b8b10c450a8a13162138
Claire Tracy <51429859+C-Tracy@users.noreply.github.com>
2023-01-31 10:29:02 -0600

    CTracy comments: Close!

--- count-fasta-seqs.sh
+++ count-fasta-seqs.sh
@@ -110,3 +110,7 @@ sum=`grep ">" $@ | wc -l`
 	echo "$sum"
 
 
+#Ctracy Comment: Close! There are a few formatting issues that are causing the tests to fail when you run "sh run_tests.sh"
+#The first issue is it is printing the filenames (look outside of the code you inserted for what line might be causing this)
+#The second issue is the indentation of your output - when you are callling echo and the value of a variable you don't need quotation marks - try editing that to see if you can fix the indentation
+
